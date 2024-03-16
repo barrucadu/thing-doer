872f2e4559cb0b6c9f3d94221e7339643d66530b
Michael Walker <mike@barrucadu.co.uk>
2024-03-16 22:53:24 +0000

    Add a default package to the flake

--- flake.nix
+++ flake.nix
@@ -1,24 +1,33 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, rust-overlay }:
+  outputs = { self, nixpkgs, gitignore, rust-overlay }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
         overlays = [ rust-overlay.overlays.default ];
       };
+      rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      rustPlatform = pkgs.makeRustPlatform {
+        cargo = rustToolchain;
+        rustc = rustToolchain;
+      };
       buildDeps = with pkgs; [
         coreutils
         clang
         protobuf
-        (rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+        rustToolchain
       ];
     in
     {
@@ -44,5 +53,27 @@
       };
 
       formatter.${system} = pkgs.nixpkgs-fmt;
+
+      packages.${system}.default = rustPlatform.buildRustPackage rec {
+        pname = "thing-doer";
+        version = "0.0.0";
+
+        src = gitignore.lib.gitignoreSource ./.;
+
+        cargoLock = {
+          lockFile = ./Cargo.lock;
+          outputHashes = {
+            "dns-resolver-0.1.0" = "sha256-Fg8fGWdE8VylpszzCXGcQ/HBjtAl9cSwfn5t9btlWpc=";
+          };
+        };
+
+        nativeBuildInputs = buildDeps;
+        doCheck = false;
+
+        meta = {
+          description = "A simple container orchestrator.";
+          homepage = "https://github.com/barrucadu/thing-doer";
+        };
+      };
     };
 }
